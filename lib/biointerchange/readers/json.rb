69f832ca9dad01fc56ecbf7412cdb505a0ec49e1
GeraintDuck <duckg@cs.man.ac.uk>
2012-09-05 10:53:06 +0900

    Added initial json and xml file parsers. Wrote initial code for starting point, using getopts etc.

---
+++ lib/biointerchange/readers/json.rb
@@ -0,0 +1,91 @@
+module BioInterchange
+
+require 'rubygems'
+require 'json'
+
+class JsonReader
+
+  def initialize(jsontype, name, name_uri, date = nil, processtype = Process::UNSPECIFIED, version = nil)
+    @jsontype = jsontype
+    @name = name
+    @name_uri = name_uri
+    @date = date
+    @processtype = processtype
+    
+    @metadata = {}
+    @metadata['version'] = version
+  end
+  
+  def serialize(inputstream)
+  
+    raise ArgumentError, 'InputStream not of type IO, cannot read.' unless inputstream.kind_of?(IO)
+  
+    @data = inputstream.read
+
+    
+    
+    if @jsontype.match(/^pubannos$/)
+      pubannos
+    end
+  end
+  
+
+
+
+private 
+
+  def pubannos
+    
+    process = Process.new(@name, @name_uri, @date, @processtype, @metadata)
+    
+    
+    result = JSON.parse(@data)
+    
+    if result.has_key? 'Error'
+      raise "Error getting and parsing JSON file"
+    end
+    
+    
+    text = result['text']
+    doc_uri = nil
+    
+    doc = Document.new(doc_uri)
+    docContent = Content.new(0, text.length, Content::DOCUMENT)
+    
+    #so our document requires content of type document or abstract
+    #should it hold the content string?
+    
+    annotations = []
+    
+    result['catanns'].each do |annot| 
+      start_offset = annot['begin']
+      end_offset = annot['end']
+      length = end_offset - start_offset
+      created_time = annot['created_at']
+      updated_time = annot['updated_at']
+      category = annot['category']
+      #annset_id = annot['annset_id']
+      #doc_id = annot['doc_id']
+      #id = annot['id']
+      
+      entity = text.slice(start_offset..end_offset)
+    
+      #phrase = type for NE
+      con = Content.new(start_offset, length, Content::PHRASE)
+      
+      annotations << con
+      
+      #set process.date = updated_time?
+    
+      #puts "'#{start_offset}' '#{end_offset}' '#{category}' '#{entity}' "
+    
+      puts annotations
+    
+    end
+    
+  end
+  
+
+end
+
+end
