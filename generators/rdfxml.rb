43c0c45c7b6d633e269508174add35f2502de979
Joachim Baran <joachim.baran@gmail.com>
2012-09-08 17:44:40 +0900

    Cleaned method names, that contained invalid characters beforehand.

--- generators/rdfxml.rb
+++ generators/rdfxml.rb
@@ -19,6 +19,10 @@ reader.each_statement { |statement|
   model[statement.subject][statement.predicate] = statement.object
 }
 
+object_properties = {}
+datatype_properties = {}
+classes = {}
+
 puts "class #{ARGV[1]}"
 puts ''
 
@@ -31,13 +35,39 @@ model.keys.each { |key|
   if type then
     if type == RDF::OWL.ObjectProperty or type == RDF::OWL.DatatypeProperty or type == RDF::OWL.Class then
       label = entry[RDF::RDFS.label].to_s
-      next if label.empty?
-      puts "    def #{label.gsub(' ', '_')}"
-      puts "      \"#{key.to_s}\""
+      generated_label = label.gsub(/[ '-.<>]/, '_').gsub(/\([^\)]*?\)/, '').sub(/^(\d+)/, "a_#{$1}").gsub(/^_+|_+$/, '').gsub(/_+/, '_')
+      next if generated_label.empty?
+      uri = key.to_s
+      puts "    def #{generated_label}"
+      puts "      RDF::URI.new('#{uri}')"
       puts '    end'
+      puts ''
+      object_properties[uri] = true if type == RDF::OWL.ObjectProperty
+      datatype_properties[uri] = true if type == RDF::OWL.DatatypeProperty
+      classes[uri] = true if type == RDF::OWL.Class
     end
   end
 }
 
+puts '    def is_object_property?(uri)'
+object_properties.keys.each { |uri| puts "      return true if uri == RDF::URI.new('#{uri}')" }
+puts '      false'
+puts '    end'
+puts ''
+
+puts '    def is_datatype_property?(uri)'
+datatype_properties.keys.each { |uri| puts "      return true if uri == RDF::URI.new('#{uri}')" }
+puts '      false'
+puts '    end'
+puts ''
+
+puts '    def is_class?(uri)'
+classes.keys.each { |uri| puts "      return true if uri == RDF::URI.new('#{uri}')" }
+puts '      false'
+puts '    end'
+puts ''
+
+puts 'end'
+puts ''
 puts 'end'
 
