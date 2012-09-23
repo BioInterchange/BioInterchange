1fc88e0927dbae3e3d75bbd6858dba637f00e042
GeraintDuck <duckg@cs.man.ac.uk>
2012-09-23 17:26:36 +0100

    Edited for new(er) exception heirarchy.

--- lib/biointerchange/exceptions.rb
+++ lib/biointerchange/exceptions.rb
@@ -1,18 +1,38 @@
 module BioInterchange::Exceptions
 
+# Top level Error class for all framework errors.
 class BioInterchangeError < StandardError
 end
 
-class FormatError < BioInterchangeError
+# Error class for issues regarding input file formats. 
+# This error class is caught at the hightest level and
+# only the error message is returned to the end user
+# (not the backtrace). This helps keep the framework
+# more user friendly. Note that this means meaningful
+# error messages are strongly encourged. 
+class InputFormatError < BioInterchangeError
 end
 
-class ReaderError < BioInterchangeError
+# Top class error for errors raised only during 
+# framework implementation and extension. Once
+# such an implementation/extension is completed,
+# these errors should no longer be possible.
+# Examples include a method reciving something it 
+# can't handle. The framework does not resuce these
+# errors leaving the backtraces for help in debug.
+class ImplementationError < BioInterchangeError
 end
 
-class ModelError < BioInterchangeError
+# Implementation errors originating from readers.
+class ImplementationReaderError < ImplementationError
 end
 
-class WriterError < BioInterchangeError
+# Implementation errors originating from models.
+class ImplementationModelError < ImplementationError
+end
+
+# Implementation errors originating from writers.
+class ImplementationWriterError < ImplementationError
 end
 
 end
--- spec/exceptions_spec.rb
+++ spec/exceptions_spec.rb
@@ -7,19 +7,19 @@ load 'lib/biointerchange/exceptions.rb'
 describe BioInterchange::Exceptions do
   describe 'error and exception creation tests' do
     it 'format error' do
-      expect { raise BioInterchange::Exceptions::FormatError }.to raise_error(BioInterchange::Exceptions::BioInterchangeError)
+      expect { raise BioInterchange::Exceptions::InputFormatError }.to raise_error(BioInterchange::Exceptions::BioInterchangeError)
     end
     
     it 'reader error' do
-      expect { raise BioInterchange::Exceptions::ReaderError }.to raise_error(BioInterchange::Exceptions::BioInterchangeError)
+      expect { raise BioInterchange::Exceptions::ImplementationReaderError }.to raise_error(BioInterchange::Exceptions::ImplementationError)
     end
     
     it 'model error' do
-      expect { raise BioInterchange::Exceptions::ModelError }.to raise_error(BioInterchange::Exceptions::BioInterchangeError)
+      expect { raise BioInterchange::Exceptions::ImplementationModelError }.to raise_error(BioInterchange::Exceptions::ImplementationError)
     end
     
     it 'writer error' do
-      expect { raise BioInterchange::Exceptions::WriterError }.to raise_error(BioInterchange::Exceptions::BioInterchangeError)
+      expect { raise BioInterchange::Exceptions::ImplementationWriterError }.to raise_error(BioInterchange::Exceptions::ImplementationError)
     end
     
   end
