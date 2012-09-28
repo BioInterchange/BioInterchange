1b6865b9d86100db5c5073ee475166d3023d0891
Joachim Baran <joachim.baran@gmail.com>
2012-09-27 22:50:58 -0400

    Updated GFF3 ontology with identifier instead of labels.

--- lib/biointerchange/gff3.rb
+++ lib/biointerchange/gff3.rb
@@ -2,82 +2,102 @@ module BioInterchange
 
 class GFF3
 
-    def self.attributes
-      RDF::URI.new('http://www.sequenceontology.org/gff3#attributes')
-    end
-
-    def self.contains
-      RDF::URI.new('http://www.sequenceontology.org/gff3#contains')
-    end
-
-    def self.end
-      RDF::URI.new('http://www.sequenceontology.org/gff3#end')
-    end
-
-    def self.parent
-      RDF::URI.new('http://www.sequenceontology.org/gff3#parent')
-    end
-
-    def self.phase
-      RDF::URI.new('http://www.sequenceontology.org/gff3#phase')
-    end
-
-    def self.score
-      RDF::URI.new('http://www.sequenceontology.org/gff3#score')
-    end
-
     def self.seqid
-      RDF::URI.new('http://www.sequenceontology.org/gff3#seqid')
+      RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0004')
     end
 
     def self.source
-      RDF::URI.new('http://www.sequenceontology.org/gff3#source')
-    end
-
-    def self.start
-      RDF::URI.new('http://www.sequenceontology.org/gff3#start')
-    end
-
-    def self.strand
-      RDF::URI.new('http://www.sequenceontology.org/gff3#strand')
-    end
-
-    def self.tag
-      RDF::URI.new('http://www.sequenceontology.org/gff3#tag')
+      RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0005')
     end
 
     def self.type
-      RDF::URI.new('http://www.sequenceontology.org/gff3#type')
+      RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0006')
+    end
+
+    def self.start
+      RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0007')
+    end
+
+    def self.end
+      RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0008')
+    end
+
+    def self.score
+      RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0009')
+    end
+
+    def self.strand
+      RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0010')
+    end
+
+    def self.phase
+      RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0011')
+    end
+
+    def self.attributes
+      RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0012')
+    end
+
+    def self.tag
+      RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0013')
+    end
+
+    def self.parent
+      RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0014')
+    end
+
+    def self.contains
+      RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0015')
     end
 
     def self.Attribute
-      RDF::URI.new('http://www.sequenceontology.org/gff3#Attribute')
+      RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0003')
     end
 
-    def self.Feature
-      RDF::URI.new('http://www.sequenceontology.org/gff3#Feature')
+    def self.Strand
+      RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0016')
+    end
+
+    def self.Positive
+      RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0017')
+    end
+
+    def self.Negative
+      RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0018')
+    end
+
+    def self.UnknownStrand
+      RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0019')
+    end
+
+    def self.NotStranded
+      RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0020')
     end
 
     def self.Set
-      RDF::URI.new('http://www.sequenceontology.org/gff3#Set')
+      RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0001')
+    end
+
+    def self.Feature
+      RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0002')
     end
 
     # Determines whether the given URI is an object property.
     #
     # +uri+:: URI that is tested for being an object property
     def self.is_object_property?(uri)
-      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#attributes')
-      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#contains')
-      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#end')
-      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#parent')
-      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#phase')
-      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#score')
-      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#seqid')
-      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#source')
-      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#start')
-      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#strand')
-      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#tag')
-      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#type')
+      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0004')
+      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0005')
+      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0006')
+      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0007')
+      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0008')
+      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0009')
+      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0010')
+      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0011')
+      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0012')
+      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0013')
+      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0014')
+      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0015')
       false
     end
 
@@ -92,9 +112,14 @@ class GFF3
     #
     # +uri+:: URI that is tested for being a class
     def self.is_class?(uri)
-      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#Attribute')
-      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#Feature')
-      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#Set')
+      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0003')
+      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0016')
+      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0017')
+      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0018')
+      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0019')
+      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0020')
+      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0001')
+      return true if uri == RDF::URI.new('http://www.sequenceontology.org/gff3#GFF3_0002')
       false
     end
 
