f2f710615ce40799d3d85c812d73e5ca43a940ad
Joachim Baran <joachim.baran@gmail.com>
2013-05-09 22:13:23 -0400

    Updated Java POM and Python egg setup to new version number.

--- supplemental/python/setup.py
+++ supplemental/python/setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
  
 setup(
     name = "biointerchange",
-    version = "0.2.1",
+    version = "0.2.2",
     author = "Joachim Baran, Geraint Duck",
-    description = ( "Wrapper for easy access to GFF3O, GVF1O, SIO, SO and SOFA.",
+    description = ( "Wrapper for easy access to FALDO, GFF3O, GVF1O, SIO, SO and SOFA.",
                     "Part of the BioInterchange project." ),
     license = "MIT",
     url = "http://www.biointerchange.org",
